162aaca3bf7e2b12505f7cc22d2876608d1d460c
Motochika Goya <you@example.com>
2018-08-02 10:03:27 +0900

    08/01アルゴリズム課題

---
+++ src/algo-p1/task20180801_q02.py
@@ -0,0 +1,9 @@
+# 計算した結果を変数に代入してみよう
+apple_price = 200
+apple_count = 8
+
+# apple_priceとapple_countを掛けた結果を、変数total_priceに代入してください
+total_price = (200 * 8)
+
+# total_priceの値を出力してください
+print(total_price)
---
+++ src/algo-p1/task20180801_q05.py
@@ -0,0 +1,9 @@
+# 型変換をしてみよう
+age = 19
+# ageを用いて「私は19歳です」と出力してください
+age1 = str(age)
+print("私は" + age1 + "歳です")
+
+count = 5
+# countに1を足した値を出力してください
+print(count + 1)
