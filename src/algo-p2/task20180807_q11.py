5d613bd8c5e00f3f722785790c22a25a1932f6f3
Motochika Goya <you@example.com>
2018-08-08 14:16:44 +0900

    08/07アルゴリズム課題

--- src/algo-p2/task20180807_q11.py
+++ src/algo-p2/task20180807_q11.py
@@ -9,10 +9,12 @@ for item_name in items:
 
     #キーと変数input_countを用いて「購入する◯◯の個数は△△個です」となるように出力してください
     s = "購入する{0}の個数は,{1}個です。".format(item_name,input_count)
+    print(s)
 
     #input_countを数値として変数countに代入してください
     count = int(input_count)
     #変数total_priceに果物1個の値段と変数countを掛けた値を代入してください
-    total_price = items(item_name) * count
+    total_price = items[item_name] * count
     #変数total_priceと型変換を用いて、「支払い金額は◯◯円です」となるように出力してください
-    print = "支払い金額は" + total_price + "円です"
+    s = ("支払い金額は" + format(total_price) + "円です")
+    print(s)
